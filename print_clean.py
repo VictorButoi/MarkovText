de491a8c6b5cd7317caeccaf0af521454a18a122
Victor Butoi <vib9@cornell.edu>
2019-12-14 01:01:45 -0500

    added some more dicts

---
+++ print_clean.py
@@ -0,0 +1,5 @@
+print("\nNow when Laertes and the two men raised their hands upon your undertaking.\n\
+Alas!\n\
+Winds from East, South, North, East, and West have breathed upon it in his boyhood.\n\
+Let us see what he has many suitors, who eat them up at the well, do not think he will, set your\n\
+Now, therefore, do you sit like that of my son, that I am afraid of such sovereign power and virtue\n")
